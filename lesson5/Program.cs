6842e58d2ae49f341954fb52b1fc491460f8ad59
dtnfktu <vtlg@mail.ru>
2022-07-06 06:47:35 +0300

    lesson5 added

---
+++ lesson5/Program.cs
@@ -0,0 +1,21 @@
+﻿// Генерация целочисленного массива
+
+int[] GenerateRandomArray(int size, int min, int max)
+{
+    int[] array = new int[size];
+
+    for (int i = 0; i < size; i++)
+    {
+        array[i] = new Random().Next(min, max + 1);
+    }
+
+    return array;
+}
+
+void ShowArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write(arr[i] + " ");
+    }
+}
